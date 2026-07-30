0537766b09bb81ccf5cf0d0c5d50c8eb812b1e6c
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-07-30 21:48:26 +0530

    add file

---
+++ Day1.java
@@ -0,0 +1,75 @@
+class Day1{
+    public static void main(String args[]){
+        // System.out.println("Welcome dude");
+
+
+        // Student Details :
+
+        // String Name ="abinaya";
+        // long regNumber = 620622;
+        // float cgpa = 7.5f;
+        // char attendence = 'P';
+
+        // System.out.println("Name : "+Name);
+        // System.out.println("reg Number : "+regNumber);
+        // System.out.println("CGPA : "+cgpa);
+        // System.out.println("Attendence : "+attendence);
+
+        // OPERATORS :
+
+        //Arithmetic Operator +,-,*,/,%
+
+       
+        // int num1 = 100;
+        // int num2 = 50;
+        // int answer = num1 % num2;
+        // System.out.println(answer);
+
+
+        //Assignment Operator  +=,-=,*=,/=,%=
+
+        
+        // int num1 = 100, num2 = 50;
+        // // num2 += num1;
+        // // num2=num2+num1
+        // num1 -= num2;
+        // // num1=num1+num2
+        // System.out.println(num1);
+
+        //Relational Operator  >,>=,<,<=,==,!=
+   
+
+        // int num1 = 100;
+        // int num2 = 50;
+        // System.out.println(num1 <= num2);
+        // System.out.println(num1 < num2);
+        // System.out.println(num1 >= num2);
+        // System.out.println(num1 > num2);
+        // System.out.println(num1 == num2);
+        // System.out.println(num1 != num2);
+        // boolean check = num1 == num2;
+        // System.out.println(check);
+
+        // Log.Operator   &&, ||, !
+
+
+        // int num1 = 100;
+        // int num2 = 50;
+        // System.out.println(!(num1 != num2));
+        // System.out.println(num1 == num2 || num1 != num2);
+        // System.out.println(num1!=num2 && num1>num2);
+
+       //3] Increment , Decrement
+
+
+         // Pre Increment ++i / Pre Decrement --i
+        // Post Increment i++ / Post Decrement i--
+        int a = 8;
+        int b = a--;
+        System.out.println(a);
+        System.out.println(b);
+
+
+       
+    }
+}
