52896f0ea18a6b4f4b68dafefb3c52066e37f6ef
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-08-03 21:17:21 +0530

    add

--- Day3.java
+++ Day3.java
@@ -1,16 +1,83 @@
- class Day3 {
-    public static void main(String[] args) {
+// class Day3 {
+   // public static void main(String[] args) {
 
-        //1-----------------FOR LOOP---------------->
-       // System.out.println("counting 1 to 5");
-        
+        //1-----------------FOR LOOP------------------->
+       // System.out.println("");
        // for(int i = 1; i <= 5; i++) {
           //  System.out.println("Number : " + i);
        // }
    // }
 //}
-for(int i = 10; i <= 50; i = i + 10){
-    System.out.println(i);
-}
-    }
- }
+
+
+//----------------EXAMPLE ------------------------------>
+//for(int i = 10; i <= 50; i = i + 10){
+   // System.out.println(i);
+//}
+   // }
+// }
+
+
+//2--------------------------WHILE LOOP------------------->
+       // System.out.println("counting 1 to 5");
+       // int i = 1;  
+       // while(i <= 5){  
+          //  System.out.println("Number : " + i);
+          //  i++;  
+       // }
+   // }
+//}
+
+
+//3-------------------------DO WHILE LOOP---------------->
+       // System.out.println("");
+       // int i = 1;  
+       // do{  
+           // System.out.println("Number : " + i);
+          //  i++;  
+       // } while(i <= 5);  
+   // }
+//}
+
+//4-------------------------BREAK-------------------------->
+      //  System.out.println("");
+       // for(int i = 1; i <= 10; i++){
+          //  if(i == 5){ 
+              //  break;  
+           // }
+           // System.out.println("Number : " + i);
+       // }
+    //}
+//}
+
+//5--------------------CONTINUE----------------------------->
+       // System.out.println("");
+        //for(int i = 1; i <= 5; i++){
+           // if(i == 3){  
+              //  continue;   
+           // }
+           // System.out.println("Number : " + i);
+       // }
+   // }
+//}
+
+//6------------------NESTED LOOP------------------------------>
+       // System.out.println("");
+       // for(int i = 1; i <= 2; i++){  
+           // for(int j = 1; j <= 3; j++){
+              //  System.out.print("* ");  
+           // }
+           // System.out.println(); 
+        //}
+   // }
+//}
+
+//----------------------EXAMPLE2------------------------------->
+       // for(int i = 1; i <= 3; i++){ 
+          //  for(int j = 1; j <= 5; j++){ 
+              //  System.out.print(i + "x" + j + "=" + (i*j) + "\t");
+           // }
+           // System.out.println(); 
+      // }
+   // }
+//}
