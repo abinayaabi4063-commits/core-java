c5c87c4a308a161b5a42e76643f50b43f890187f
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-08-03 19:52:02 +0530

    add

--- Day3.java
+++ Day3.java
@@ -1,10 +1,16 @@
  class Day3 {
     public static void main(String[] args) {
+
+        //1-----------------FOR LOOP---------------->
+       // System.out.println("counting 1 to 5");
         
-        System.out.println("counting 1 to 5");
-        
-        for(int i = 1; i <= 5; i++) {
-            System.out.println("Number : " + i);
-        }
-    }
+       // for(int i = 1; i <= 5; i++) {
+          //  System.out.println("Number : " + i);
+       // }
+   // }
+//}
+for(int i = 10; i <= 50; i = i + 10){
+    System.out.println(i);
 }
+    }
+ }
