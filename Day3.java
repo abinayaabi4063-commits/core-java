5ae14054236385398ec81ad95ade1f27b1f85369
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-08-03 19:39:21 +0530

    add

---
+++ Day3.java
@@ -0,0 +1,11 @@
+class3{
+     public static void main (String args []){
+        
+        System.out.println("--- COUNTING 1 TO 5 ---");
+        
+        for(int i = 1; i <= 5; i++){  
+            System.out.println("Number : " + i);
+        }
+        
+    }
+}
