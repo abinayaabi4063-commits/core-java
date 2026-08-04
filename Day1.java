81cc696632c3334413c62974f1a4ca2766eb1dce
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-08-04 18:36:39 +0530

    add

--- Day1.java
+++ Day1.java
@@ -1,34 +1,27 @@
-class Day1{
-    public static void main(String args[]){
+//class Day1{
+   // public static void main(String args[]){
         // System.out.println("Welcome dude");
 
-
-        // Student Details :
-
+        //1---------------- Student Details :--------------------->
         // String Name ="abinaya";
         // long regNumber = 620622;
         // float cgpa = 7.5f;
         // char attendence = 'P';
-
         // System.out.println("Name : "+Name);
         // System.out.println("reg Number : "+regNumber);
         // System.out.println("CGPA : "+cgpa);
         // System.out.println("Attendence : "+attendence);
 
-        // OPERATORS :
 
+        //2------------------ OPERATORS :--------------------------->
         //Arithmetic Operator +,-,*,/,%
-
-       
         // int num1 = 100;
         // int num2 = 50;
         // int answer = num1 % num2;
         // System.out.println(answer);
 
 
-        //Assignment Operator  +=,-=,*=,/=,%=
-
-        
+        //3----------------Assignment Operator  +=,-=,*=,/=,%=------>
         // int num1 = 100, num2 = 50;
         // // num2 += num1;
         // // num2=num2+num1
@@ -36,9 +29,8 @@ class Day1{
         // // num1=num1+num2
         // System.out.println(num1);
 
-        //Relational Operator  >,>=,<,<=,==,!=
-   
 
+        //4--------------Relational Operator  >,>=,<,<=,==,!=-------->
         // int num1 = 100;
         // int num2 = 50;
         // System.out.println(num1 <= num2);
@@ -50,26 +42,41 @@ class Day1{
         // boolean check = num1 == num2;
         // System.out.println(check);
 
-        // Log.Operator   &&, ||, !
 
-
+        //5-----------------Log.Operator   &&, ||, !-------------------->
         // int num1 = 100;
         // int num2 = 50;
         // System.out.println(!(num1 != num2));
         // System.out.println(num1 == num2 || num1 != num2);
         // System.out.println(num1!=num2 && num1>num2);
 
-       //3] Increment , Decrement
+
+       //6------------------ Increment , Decrement----------------------->
+         // PRE INCREMENT++I:
+       // int i = 5;
+       // int j = ++i;  
+       // System.out.println("i = " + i);  
+       // System.out.println("j = " + j);
 
 
-         // Pre Increment ++i / Pre Decrement --i
-        // Post Increment i++ / Post Decrement i--
-        int a = 8;
-        int b = a--;
-        System.out.println(a);
-        System.out.println(b);
+       //PRE DECREAMENT--I: 
+       // int i = 5;
+        //int j = --i;  
+       // System.out.println("i = " + i);  
+       // System.out.println("j = " + j); 
 
 
-       
-    }
-}
+       //POST INCREAMENTI++:
+       // int i = 5;
+       // int j = i++;  
+       // System.out.println("i = " + i);  
+      //  System.out.println("j = " + j); 
+
+      
+      //POST DECREAMENTI--:
+       // int i = 5;
+       // int j = i--; 
+       // System.out.println("i = " + i);  
+       // System.out.println("j = " + j);     
+   // }
+//}
